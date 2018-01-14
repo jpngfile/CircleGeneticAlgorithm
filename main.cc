dbb5e7333c8e358cd83401ef1aa8bef42e80de45
Jason P'ng <jsjpng@gmail.com>
2018-01-14 02:08:14 -0800

    Made random circles

--- main.cc
+++ main.cc
@@ -1,10 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    srand(time(NULL));
 
+    std::vector<sf::CircleShape> circles;
+    for (int i = 0; i < 15; i++){
+        sf::CircleShape shape(10.f);
+        shape.setFillColor(sf::Color::Green);
+        shape.setPosition(rand() % 500, rand() % 500);
+        circles.push_back(shape);
+    }
     while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
@@ -13,7 +22,9 @@ int main() {
         }
 
         window.clear();
-        window.draw(shape);
+        for (sf::CircleShape shape : circles){
+            window.draw(shape);
+        }
         window.display();
     }
 
